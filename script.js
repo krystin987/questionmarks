907b3d5d9723159d116e70be82f0d80acd6632cf
krystin villeneuve <krystinv@gmail.com>
2018-05-11 14:33:19 -0600

    updated for comments

--- script.js
+++ script.js
@@ -27,36 +27,36 @@ console.timeEnd();
 
 // function solution found on google
 // function QuestionsMarks(str) {
-//   res = false;
-//   for(let i = 0; i < str.length; i++) {
-//       for(let j = i+1; j < str.length; j++) {
-//           if(Number(str[i])+Number(str[j]) === 10) {
-//             if(str.slice(i+1,j).split('?').length - 1 === 3) {
-//                 res = true;
-//                 break;
+//   res = false; // this boolean is redundant and unnecessary
+//   for(let i = 0; i < str.length; i++) { // outer loop
+//       for(let j = i+1; j < str.length; j++) { // inner loop
+//           if(Number(str[i])+Number(str[j]) === 10) { // checks integers from input string for value = 10
+//             if(str.slice(i+1,j).split('?').length - 1 === 3) { // checks for '?' between the integers to see if there are 3
+//                 res = true; // unnecessary boolean reassignment
+//                 break; //unnecessary break
 //             } else {
-//                 return false;
+//                 return false; // unnecessary boolean return and else statement
 //             }
 //           }
 //       }
 //   }
-//   return res;
+//   return res; // unnecessary reassigned boolean return
 // }
 
 // const QuestionMarks = (str) => {
-//   n = str.length // stored for efficiency
-//   for(var i = 0; i < n; i++) {
-//     for(var j = i; j < n; j++) {
-//         if(Number(str[i])+Number(str[j]) === 10) {
-//           if(str.slice(i+1,j).split('?').length - 1 === 3) return true;
+//   n = str.length // removed boolean, stored input length
+//   for(var i = 0; i < n; i++) { // outer loop
+//     for(var j = i; j < n; j++) { // inner loop
+//         if(Number(str[i])+Number(str[j]) === 10) { checks integers for added value = 10
+//           if(str.slice(i+1,j).split('?').length - 1 === 3) return true; //checks shallow copy for 3 '?' between integers that added to 10
 //         }
 //     }
 //   }
-//   return false;
+//   return false; // returns when condition in the first if statement for integer values fails or when the chars between the integers that add to 10 don't have 3 question marks
 // }
 
 
-/* This section has the calls for timing embedded, the difference seems negligible */
+/* This section is identical to the above function, but has the calls for timing embedded, the difference seems negligible */
 
 // const QuestionMarks = (str) => {
 //   console.time("timer");
