32147313ff9021662bdada071488c7c371850b09
krystin villeneuve <krystinv@gmail.com>
2018-05-11 13:10:15 -0600

    update to have 2 versions, one with timers embedded, one with timers wrapping function calls

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
+/* The first set has the timer embedded in the function, the next set is meant to run when the page loads*/
+
+// const QuestionMarks = (str) => {
+//   console.time("timer");
+//   n = str.length
+//   for(var i = 0; i < n; i++) {
+//     for(var j = i; j < n; j++) {
+//         if(Number(str[i])+Number(str[j]) === 10) {
+//           if(str.slice(i+1,j).split('?').length - 1 === 3) return true;
+//         }
+//     }
+//   console.timeEnd("timer");
+//   }
+//   console.timeEnd("timer");
+//   return false;
+// }
+
 const QuestionMarks = (str) => {
-  console.time("timer");
-  n = str.length
+  n = str.length // stored for efficiency
   for(var i = 0; i < n; i++) {
     for(var j = i; j < n; j++) {
         if(Number(str[i])+Number(str[j]) === 10) {
           if(str.slice(i+1,j).split('?').length - 1 === 3) return true;
         }
     }
-  console.timeEnd("timer");
   }
-  console.timeEnd("timer");
   return false;
 }
+
+// true is slower than false, as true actually runs through the inner loop and false just dumps out
+console.time("timer");
+QuestionMarks("bcc?7??ccc?3tt1??????5");
+console.timeEnd("timer");
+
+console.time("timer");
+QuestionMarks("alsdkf")
+console.timeEnd("timer");
