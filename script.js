965ecb41ac143a18a27f4b3fcae3eb93e0dc0fd6
krystin villeneuve <krystinv@gmail.com>
2018-05-11 11:50:38 -0600

    final

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const QuestionMarks = (str) => {
-  for(var i in str) {
-    for(var j in str) {
+  console.time("timer");
+  n = str.length
+  for(var i = 0; i < n; i++) {
+    for(var j = i; j < n; j++) {
         if(Number(str[i])+Number(str[j]) === 10) {
           if(str.slice(i+1,j).split('?').length - 1 === 3) return true;
         }
-      }
     }
+    console.timeEnd("timer");
+  }
   return false;
 }
